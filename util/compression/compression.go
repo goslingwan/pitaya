560f77b6132fd4c618c44c59941b327b8626bd68
wenchange1989 <wenchange1989@gmail.com>
2020-08-14 19:26:23 +0800

    Optimized compression by sync.pool

--- util/compression/compression.go
+++ util/compression/compression.go
@@ -4,17 +4,47 @@ import (
 	"bytes"
 	"compress/zlib"
 	"io/ioutil"
+	"sync"
 )
 
+var (
+	// 公共对象池,更极致的优化可以建多个池
+	zlibPool = sync.Pool{New: func() interface{} {
+		buf := new(bytes.Buffer)
+		return zlib.NewWriter(buf)
+	}}
+	bufPool = sync.Pool{New: func() interface{} {
+		return new(bytes.Buffer)
+	}}
+)
+
+func init() {
+	for i := 0; i < 5000; i++ {
+		buf := new(bytes.Buffer)
+		bufPool.Put(buf)
+		z := zlib.NewWriter(buf)
+		zlibPool.Put(z)
+	}
+}
+
 func DeflateData(data []byte) ([]byte, error) {
-	var bb bytes.Buffer
-	z := zlib.NewWriter(&bb)
+	buf := bufPool.Get().(*bytes.Buffer)
+	z := zlibPool.Get().(*zlib.Writer)
+	z.Reset(buf)
+	defer func() {
+		// 归还buff
+		buf.Reset()
+		bufPool.Put(buf)
+		// 归还Writer
+		zlibPool.Put(z)
+	}()
+	z.Reset(buf)
 	_, err := z.Write(data)
 	if err != nil {
 		return nil, err
 	}
 	z.Close()
-	return bb.Bytes(), nil
+	return buf.Bytes(), nil
 }
 
 func InflateData(data []byte) ([]byte, error) {
@@ -29,13 +59,13 @@ func InflateData(data []byte) ([]byte, error) {
 
 func IsCompressed(data []byte) bool {
 	return len(data) > 2 &&
-	(
+		(
 		// zlib
 		(data[0] == 0x78 &&
-		(data[1] == 0x9C ||
-		data[1] == 0x01 ||
-		data[1] == 0xDA ||
-		data[1] == 0x5E)) ||
-		// gzip
-		(data[0] == 0x1F && data[1] == 0x8B))
+			(data[1] == 0x9C ||
+				data[1] == 0x01 ||
+				data[1] == 0xDA ||
+				data[1] == 0x5E)) ||
+			// gzip
+			(data[0] == 0x1F && data[1] == 0x8B))
 }
